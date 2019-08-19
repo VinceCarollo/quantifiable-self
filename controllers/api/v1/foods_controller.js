79297899ef204e7db361024bd64dcd665509b21c
Vince Carollo <vincecarollo@gmail.com>
2019-08-19 17:27:14 -0600

    Add foods controller and food index enpoint
    Co-Authored-By: Patrick Duvall <duvallp208@gmail.com>

--- controllers/api/v1/foods_controller.js
+++ controllers/api/v1/foods_controller.js
@@ -1,9 +1,17 @@
+var Food = require('../../../models').Food;
+
 var showFoods = function (req, res) {
-console.log("HI")
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).send();
+  Food.findAll()
+    .then(foods => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(foods));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    })
 }
 
 module.exports = {
-index: showFoods
+  index: showFoods
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+process.env.NODE_ENV = 'test';
 var assert = require('assert');
 var expect = require('chai').expect;
 var should = require('chai').should();
@@ -5,21 +6,44 @@ var should = require('chai').should();
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var app = require('../app');
-var Food = require('../../../models').Food;
+var Food = require('../models').Food;
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
+
 describe("Foods", () => {
+  afterEach(() => {
+    Food.destroy({
+      where: {},
+      truncate: true
+    })
+  })
     describe("GET /api/v1/foods", () => {
         // Test to get all students record
         it("should get all foods record", (done) => {
-             chai.request(app)
-                 .get('/api/v1/foods')
-                 .end((err, res) => {
-                     res.should.have.status(200);
-                     res.body.should.be.a('object');
-                     done();
-                  });
+          Food.bulkCreate([
+            {
+              name: 'pizza',
+              calories: 600
+            },
+            {
+              name: 'salad',
+              calories: 100
+            }
+          ]
+        )
+        .then(() => {
+          chai.request(app)
+          .get('/api/v1/foods')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body[0].should.be.a('object');
+            res.body[0].name.should.equal('pizza')
+            res.body[0].calories.should.equal(600)
+            res.body.should.have.lengthOf(2);
+            done();
+          });
+        })
          });
         // Test to get single student record
         xit("should get a single food record", (done) => {
