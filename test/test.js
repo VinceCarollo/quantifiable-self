8805d8dc649a81203c1f50aeae92a52c69a1fc49
Patrick Duvall <duvallp208@gmail.com>
2019-08-19 15:45:56 -0600

    Initial setup of mocha chai

---
+++ test/test.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+var should = require('chai').should();
+// Import the dependencies for testing
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../app');
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+describe("Foods", () => {
+    describe("GET /api/v1/foods", () => {
+        // Test to get all students record
+        it("should get all foods record", (done) => {
+             chai.request(app)
+                 .get('/api/v1/foods')
+                 .end((err, res) => {
+                     res.should.have.status(200);
+                     res.body.should.be.a('object');
+                     done();
+                  });
+         });
+        // Test to get single student record
+        xit("should get a single food record", (done) => {
+             const id = 1;
+             chai.request(app)
+                 .get(`/${id}`)
+                 .end((err, res) => {
+                     res.should.have.status(200);
+                     res.body.should.be.a('object');
+                     done();
+                  });
+         });
+
+        // Test to get single student record
+        xit("should not get a single student record", (done) => {
+             const id = 5;
+             chai.request(app)
+                 .get(`/${id}`)
+                 .end((err, res) => {
+                     res.should.have.status(404);
+                     done();
+                  });
+         });
+    });
+});
