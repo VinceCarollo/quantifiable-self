c439cdd05776f394ea656661cbb9e7338939f4c2
Patrick Duvall <duvallp208@gmail.com>
2019-08-21 15:17:37 -0600

    Add show route for one meal with test and sad path
    Co-Authored-By: Vince Carollo <vincecarollo@gmail.com>

--- routes/api/v1/meals/index.js
+++ routes/api/v1/meals/index.js
@@ -2,5 +2,6 @@ var router = require('express').Router();
 var mealsController = require('../../../../controllers/api/v1/meals_controller')
 
 router.get('/', mealsController.index)
+router.get('/:id/foods', mealsController.show)
 
 module.exports = router;
