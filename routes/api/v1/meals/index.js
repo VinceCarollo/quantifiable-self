15475e4afb2193e56ae6be2b2564c7278dedecc1
Patrick Duvall <duvallp208@gmail.com>
2019-08-21 16:28:11 -0600

    Add delete food from meal endpoint with tests and sadpath
    Co-Authored-By: Vince Carollo <vincecarollo@gmail.com>

--- routes/api/v1/meals/index.js
+++ routes/api/v1/meals/index.js
@@ -4,5 +4,6 @@ var mealsController = require('../../../../controllers/api/v1/meals_controller')
 router.get('/', mealsController.index)
 router.get('/:id/foods', mealsController.show)
 router.post('/:id/foods/:food_id', mealsController.addFood)
+router.delete('/:id/foods/:food_id', mealsController.removeFood)
 
 module.exports = router;
