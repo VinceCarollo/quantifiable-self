83cbc0a1a0db14caba33af17d54d5e57901f5d4c
Vince Carollo <vincecarollo@gmail.com>
2019-08-22 15:12:12 -0600

    Refactor food presenter to not use 'dataValues' attribute

--- pojos/food_presenter.js
+++ pojos/food_presenter.js
@@ -1,14 +1,9 @@
 class FoodPresenter {
   constructor(data) {
-    // console.log(data)
-    // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    this.id = data.dataValues.id
-    this.name = data.dataValues.name
-    this.calories = data.dataValues.calories
-
+    this.id = data.id
+    this.name = data.name
+    this.calories = data.calories
   }
-
-
 }
 
 module.exports = FoodPresenter;
