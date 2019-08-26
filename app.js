252b96ae3cbf27289dc50dce24137681b87bf6d2
Patrick Duvall <duvallp208@gmail.com>
2019-08-26 17:53:45 -0600

    Add average calories for a food type endpoint

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var foodsRouter = require('./routes/api/v1/foods/index');
 var mealsRouter = require('./routes/api/v1/meals/index');
 var recipesRouter = require('./routes/api/v1/recipes/index');
+var recipesAverageCaloriesRouter = require('./routes/api/v1/recipes/average_calories/index');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/v1/foods', foodsRouter);
 app.use('/api/v1/meals', mealsRouter);
+app.use('/api/v1/recipes/average_calories', recipesAverageCaloriesRouter);
 app.use('/api/v1/recipes', recipesRouter);
 
 
